0e2784e661de0781d7c86d5fffeba8b8690558e3
Apprentice <apprentice@dbc-chicago-009.local>
2013-08-07 17:46:32 -0500

    working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,33 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/sign_up' do
+ 
+  erb :sign_up
+end
+
+get '/sign_out' do
+  session.clear
+  redirect ('/')
+end
+
+get '/secret' do
+  if session[:user_id]
+    erb :secret 
+  else
+    erb :index
+  end
+end
+
+post '/sign_up' do
+  @user = User.new(params[:user])
+  @user.save!
+  redirect('/')
+end
+
+post '/sign_in' do
+  @user = User.authenticate(params[:email], params[:password])
+  session[:user_id] = @user.id
+  redirect('/')
+end
